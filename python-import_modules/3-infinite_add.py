a76ecc2d042e42262b6881462d2583d4603b8152
internashionalist <taylor.thames@atlasschool.com>
2024-08-28 23:44:53 -0500

    3.1 - seems too simple but hey

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys  # import sys module
 
- 
+    total = 0  # initialize total to 0
+    for arg in sys.argv[1:]:  # skip script name
+        total += int(arg)  # add argument to total
+    
+    print(total)  # print total
