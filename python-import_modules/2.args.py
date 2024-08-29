cef307a8e357da7f1533e0f5ae2e60b7ca1c633d
internashionalist <taylor.thames@atlasschool.com>
2024-08-28 23:01:56 -0500

    2.1 - 0 and 1

--- python-import_modules/2.args.py
+++ python-import_modules/2.args.py
@@ -3,3 +3,8 @@ if __name__ == "__main__":
     import sys  # import sys module
     argv = sys.argv[1:]  # get arguments from command line
     argc = len(argv)  # get number of arguments
+
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
