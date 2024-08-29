40fc1443a1d6b1ee082647ec1728e29d48fcd7c5
internashionalist <taylor.thames@atlasschool.com>
2024-08-28 22:51:40 -0500

    1.1 - smooth

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-       from calculator_1 import
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
+    print("{0} - {1} = {2}".format(a, b, sub(a, b)))
+    print("{0} * {1} = {2}".format(a, b, mul(a, b)))
+    print("{0} / {1} = {2}".format(a, b, div(a, b)))
