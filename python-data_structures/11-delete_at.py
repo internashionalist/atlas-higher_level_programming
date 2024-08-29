4b128328912093f17098816c72d1d050fdc345ae
internashionalist <taylor.thames@atlasschool.com>
2024-08-29 09:58:29 -0500

    11.1 - runs

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-
+    if idx < 0 or idx >= len(my_list):  # if index out of range
+        return my_list  # return list as is
+    del my_list[idx]  # delete element at index
+    return my_list  # return modified list
