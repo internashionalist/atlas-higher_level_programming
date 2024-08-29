613c13edd4945f099f971ba8af23153dc25f791b
internashionalist <taylor.thames@atlasschool.com>
2024-08-29 07:40:12 -0500

    8.1 - pycode

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 
 def multiple_returns(sentence):
     if len(sentence) == 0:  # if sentence is empty
-        return (0, None) # return 0 and None
+        return (0, None)  # return 0 and None
     else:  # return length of sentence and first character
         return (len(sentence), sentence[0])
