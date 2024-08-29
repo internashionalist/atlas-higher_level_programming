51aad864be45aebff50d6ba3e3660fc1560e4127
internashionalist <taylor.thames@atlasschool.com>
2024-08-28 23:45:27 -0500

    3.2 - pycode

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     total = 0  # initialize total to 0
     for arg in sys.argv[1:]:  # skip script name
         total += int(arg)  # add argument to total
-    
+
     print(total)  # print total
