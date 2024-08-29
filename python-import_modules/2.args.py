f212d1baa83f2b8cf27ee12cb6210c7b78b5af34
internashionalist <taylor.thames@atlasschool.com>
2024-08-28 23:00:31 -0500

    2.0 - base + beginning

---
+++ python-import_modules/2.args.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys  # import sys module
+    argv = sys.argv[1:]  # get arguments from command line
+    argc = len(argv)  # get number of arguments
