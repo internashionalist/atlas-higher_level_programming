45951fa49a542d6677bc35b57f6f7a03605bf0dc
internashionalist <taylor.thames@atlasschool.com>
2024-08-28 21:58:51 -0500

    9.1 - pycode

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,4 +3,4 @@
 def print_last_digit(number):
     last_digit = abs(number) % 10  # get last digit using modulo
     print(last_digit, end='')  # print last digit
-    return last_digit # return it
+    return last_digit  # return it
