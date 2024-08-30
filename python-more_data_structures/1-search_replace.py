1ba9bd37de7326dda45e24d7ae08b7db6575eb27
internashionalist <taylor.thames@atlasschool.com>
2024-08-30 01:31:41 -0500

    1.1 - find and replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
+    new_list = my_list.copy()  # copy list
+    for idx, num in enumerate(new_list):  # iterate through list
+        if num == search:  # when search is found
+            new_list[idx] = replace  # replace it
+    return new_list  # return modified list
