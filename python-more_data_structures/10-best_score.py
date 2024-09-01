3cb2269dc298841c3532d529c00197696b57f340
internashionalist <taylor.thames@atlasschool.com>
2024-08-31 20:56:18 -0500

    10.4 - pycode

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,11 +3,11 @@
 def best_score(a_dictionary):
     if not a_dictionary:  # if dictionary is empty or None
         return None  # return None
-    
+
     winner = None  # gamer with the highest score
     high_score = 0  # highest score
-    
-    for key, value in a_dictionary.items(): # go through scores
+
+    for key, value in a_dictionary.items():  # go through scores
         if value > high_score:  # if higher score found
             high_score = value  # update high_score
             winner = key  # that gamer is the winner
