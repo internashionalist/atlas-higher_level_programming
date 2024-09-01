4e44ae1533f047985789b0a128d801b16d929cc9
internashionalist <taylor.thames@atlasschool.com>
2024-08-31 20:16:23 -0500

    7.1 - update key-value pair

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value  # add or update key-value pair
+    return a_dictionary  # return updated dictionary
