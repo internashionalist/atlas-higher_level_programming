14112ccd69c51736078cb2bbcd2c52e9f796a575
internashionalist <taylor.thames@atlasschool.com>
2024-08-31 21:12:36 -0500

    12.1 - null case, dictionary, init

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0  # if not a string or None, return 0
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                      'D': 500, 'M': 1000}  # dictionary of roman numerals
+    roman = 0  # initialize roman number to 0
+
