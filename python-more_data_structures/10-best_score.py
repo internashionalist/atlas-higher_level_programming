12fc5577476df52bd99af72d3eee6e4e1ff80264
internashionalist <taylor.thames@atlasschool.com>
2024-08-31 20:49:50 -0500

    10.2 - init variables

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,4 +4,7 @@ def best_score(a_dictionary):
     if not a_dictionary:  # if dictionary is empty or None
         return None  # return None
     
- 
+    winner = None  # gamer with the highest score
+    high_score = 0  # highest score
+    
+
