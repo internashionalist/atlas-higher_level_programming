edf3f23cbe9fdd812864cde8c577c892ae4fa103
internashionalist <taylor.thames@atlasschool.com>
2024-08-31 20:38:20 -0500

    9.1 - copy, multiply, return

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.copy()  # copy dictionary
+    for key in new_dict:  # iterate over keys in dictionary
+        new_dict[key] *= 2  # multiply value by 2
+    return new_dict  # return updated dictionary
