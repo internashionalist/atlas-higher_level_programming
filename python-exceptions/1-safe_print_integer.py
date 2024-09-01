1d9efb7e6144dddf1625a7412e5139f49205085a
internashionalist <taylor.thames@atlasschool.com>
2024-08-31 22:34:51 -0500

    0.1 - weird print

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))  # print integer
+        return True  # return True if successful
+    except:
+        return False  # otherwise, return False
