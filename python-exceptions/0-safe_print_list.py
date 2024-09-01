85b823fe71fa2a6ed6749ab5d7f555077a5b1230
internashionalist <taylor.thames@atlasschool.com>
2024-08-31 22:30:35 -0500

    0.1 - try and except suck

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    elements = 0  # initialize count
+    for i in range(x):  # iterate through x
+        try:
+            print(my_list[i], end="")  # print element
+            elements += 1  # increment count
+        except IndexError:  # if index error
+            pass  # pass, stop iteration
+    print()  # print newline
+    return elements  # return count
