e953af6162e64b7057134409fe3d01c3cac9976a
internashionalist <taylor.thames@atlasschool.com>
2024-08-31 20:25:27 -0500

    8.1 - if and del

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:  # if key exists in dictionary
+        del a_dictionary[key]  # delete key-value pair
+    return a_dictionary  # return updated dictionary
