45c99113b3eddee413e883a53a0449856ea03d20
internashionalist <taylor.thames@atlasschool.com>
2024-08-31 20:55:45 -0500

    10.3 - iterate through scores, find winner

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -7,4 +7,9 @@ def best_score(a_dictionary):
     winner = None  # gamer with the highest score
     high_score = 0  # highest score
     
-
+    for key, value in a_dictionary.items(): # go through scores
+        if value > high_score:  # if higher score found
+            high_score = value  # update high_score
+            winner = key  # that gamer is the winner
+    
+    return winner  # return gamer with highest score
