3248c4268a684e098d720e4612ecb395b1a7a529
internashionalist <taylor.thames@atlasschool.com>
2024-09-01 21:15:41 -0500

    3.1 - zero case

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
+    try:
+        result = a / b  # try dividing a by b
+    except ZeroDivisionError:  # if b is 0
+        result = None  # set result to None
