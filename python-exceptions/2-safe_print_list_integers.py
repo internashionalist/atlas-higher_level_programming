80564508422a53cd61f43e757355b5acebc8958d
internashionalist <taylor.thames@atlasschool.com>
2024-09-01 21:08:51 -0500

    2.1 - gotta remember error cases

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
+    integers = 0  # initialize count
+    for i in range(x):  # iterate through x
+        try:
+            print("{:d}".format(my_list[i]), end="")  # print integer
+            integers += 1  # increment count
+        except (ValueError, TypeError):  # if error
+            continue  # skip that element
+        except IndexError:  # if index is out of range
+            break  # stop
+    print()  # print newline
+    return integers  # return count
