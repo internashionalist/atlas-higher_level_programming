50153063d66e293dded8c6e9561c41104083cdd8
internashionalist <taylor.thames@atlasschool.com>
2024-09-01 21:39:52 -0500

    4.3 - annndd done

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -14,3 +14,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:  # if out of range
             print("out of range")
             result = 0
+        finally:
+            result_list.append(result)  # append result
+    return result_list  # return result list
