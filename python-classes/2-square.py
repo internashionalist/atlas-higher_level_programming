2b20ac2a08d13b4a61f1d735f02bb7d384c7ccfa
internashionalist <taylor.thames@atlasschool.com>
2024-09-05 00:58:46 -0500

    2.0 - base (1-square.py)

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+this module defines geometric class Square
+"""
+
+
+class Square:
+    """
+    class represents a square
+
+    attributes:
+        size (int): length of the sides of a square
+    """
+
+    def __init__(self, size=0):
+        """
+        initialize new instance of Square
+        """
