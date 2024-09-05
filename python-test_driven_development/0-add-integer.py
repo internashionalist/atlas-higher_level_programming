eeaccf5c4e4953407df111da44bcaa2624cfa162
“internationalist” <taylor.thames@atlasschool.com>
2024-09-05 15:16:48 -0500

    0.0 - base

---
+++ python-test_driven_development/0-add-integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+this module's function adds two integers
+"""
+
+
+def add_integer(a, b=98):
+    """
+    This function adds two integers together.
+
+    Parameters:
+    a (int): the first integer
+    b (int): the second integer - default is 98
+
+    Raises:
+    TypeError: if a or b are not integers or floats
+
+    Returns:
+    int: the sum of a and b
+    """
