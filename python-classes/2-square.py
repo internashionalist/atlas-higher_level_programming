8604c687d927fa2b8946f5b657c907d0b8c75097
internashionalist <taylor.thames@atlasschool.com>
2024-09-05 01:04:00 -0500

    2.1 - okay this might not be so bad

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-this module defines geometric class Square
+this module defines geometric class square
 """
 
 
@@ -16,4 +16,16 @@ class Square:
     def __init__(self, size=0):
         """
         initialize new instance of Square
+
+        args: size (int): length of the sides of a square
+
+        raises:
+            TypeError: if size is not an integer
+            ValueError: if size is negative
         """
+        if not isinstance(size, int):  # check if size is not an integer
+            raise TypeError("size must be an integer")
+        elif size < 0:  # check if size is negative
+            raise ValueError("size must be >= 0")
+        else:  # assign size to instance
+            self.__size = size  # keep size private
