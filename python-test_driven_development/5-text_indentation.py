9be8b849a3c00b8a337241e80fa5b1168f906520
“internationalist” <taylor.thames@atlasschool.com>
2024-09-09 08:20:25 -0500

    4.0 - base and doc

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+5-text_indentation module
+"""
+
+
+def text_indentation(text):
+    """
+    Function that prints a text with 2 new lines
+    after every ".", "?", and ":" character
+    
+    Args:
+        text (str): text to print
+
+    Raises:
+        TypeError:  if text is not a string
+
+    Returns:
+        None
+    """
+    
