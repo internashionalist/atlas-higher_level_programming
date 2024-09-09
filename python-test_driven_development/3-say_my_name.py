36b8d56343a14433f014d3f472f28a0cdc51d56b
“internationalist” <taylor.thames@atlasschool.com>
2024-09-09 07:18:18 -0500

    3.0 - base

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+Function prints "My name is <first_name> <last_name>"
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Function prints "My name is <first_name> <last_name>" using two strings
+
+    Args:
+        first_name (str):   first name
+        last_name (str):    last name - default is ""
+
+    Raises:
+        TypeError:  if first_name or last_name are not strings
+
+    Returns:
+        None
+    """
