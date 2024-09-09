906cc08c3560133ae8a4e997bc340f9a995f00af
“internationalist” <taylor.thames@atlasschool.com>
2024-09-09 07:28:20 -0500

    2.3 - pycode will thank me

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -26,19 +26,19 @@ def matrix_divided(matrix, div):
         not all(isinstance(row, list) for row in matrix) or
         not all(isinstance(element, (int, float)) for row in matrix for element in row)):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+
     # if rows are not of equal length
     row_len = len(matrix[0])
     if not all(len(row) == row_len for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
-    
+
     # if div is not an int/float
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    
+
     # if div is 0
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
+
     # return brand new matrix with all elements divided by div (2 decimal places)
     return [[round(element / div, 2) for element in row] for row in matrix]
