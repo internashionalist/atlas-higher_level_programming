9c897465ac06230d4ba32d94ef91b660598ed903
“internationalist” <taylor.thames@atlasschool.com>
2024-09-09 07:47:33 -0500

    4.0 - base and doc but no tests

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+4-print_square module
+"""
+
+
+def print_square(size):
+    """
+    Prints a square of size x size with the character #
+
+    Args:
+        size (int): size of the printed square
+
+    Raises:
+        TypeError: if size is not an int
+        ValueError: if size is negative
+
+    Returns:
+        None
+    """
