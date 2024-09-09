7d3f36e48e75cac52329b280a08de1759783d569
“internationalist” <taylor.thames@atlasschool.com>
2024-09-09 14:53:53 -0500

    2.11 - pycode

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -31,7 +31,7 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(first_name, str) and not isinstance(last_name, str):
         raise TypeError(
             "say_my_name() missing 1 required positional"
-             " argument: 'first_name'")
+            " argument: 'first_name'")
 
     # print concatenated string
     print("My name is {} {}".format(first_name, last_name))
