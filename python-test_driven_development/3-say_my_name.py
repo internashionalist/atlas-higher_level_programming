004501d38ee142c0ce33b570a0f8e4476a35b8c6
“internationalist” <taylor.thames@atlasschool.com>
2024-09-09 14:53:05 -0500

    2.10 - wrap line for pycode

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -30,7 +30,8 @@ def say_my_name(first_name, last_name=""):
     # no arguments
     if not isinstance(first_name, str) and not isinstance(last_name, str):
         raise TypeError(
-            "say_my_name() missing 1 required positional argument: 'first_name'")
+            "say_my_name() missing 1 required positional"
+             " argument: 'first_name'")
 
     # print concatenated string
-    print ("My name is {} {}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
