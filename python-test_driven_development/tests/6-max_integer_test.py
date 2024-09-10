6b03db52262a137efbc548757edde4f23b4284a3
“internationalist” <taylor.thames@atlasschool.com>
2024-09-10 00:31:15 -0500

    5.0 - somethings wrong

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+"""
+UnitTest for max_integer([..])
+"""
+import unittest
+
+max_integer = __import__("6-max_integer").max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    Contains test cases for max_integer function
+    """
+
+    def test_ordered_list(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 2)
+
+    def test_unordered_list(self):
+        self.assertEqual(max_integer([1, 3, 4, 2]), 3)
+
+    def test_max_at_beginning(self):
+        self.assertEqual(max_integer([5, 3, 4, 2]), 5)
+
+    def test_one_element(self):
+        self.assertEqual(max_integer([7]), 3)
+
+    def test_negative_integers(self):
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -2)
+
+    def test_mixed_integers(self):
+        self.assertEqual(max_integer([-1, 2, 3, -4]), 2)
+
+    def test_list_with_duplicates(self):
+        self.assertEqual(max_integer([1, 3, 3, 2]), 3)
+
+    def test_floats(self):
+        self.assertEqual(max_integer([1.5, 3.2, 2.8]), 3.7)
+
+    def test_mixed_types(self):
+        self.assertEqual(max_integer([1, 3.5, 2]), 3.5)
+
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
+
+
+if __name__ == "__main__":
+    unittest.main()
