37fb0c2d0fbc1362a1994f7f0729ad4ddb31a8b4
“internashionalist <taylor.thames@atlasschool.com>
2024-09-10 05:56:19 -0500

    5.2 - pycode

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -67,7 +67,7 @@ class Rectangle:
 
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
-    
+
     def __del__(self):
         print("Bye rectangle...")
 
