3be64b265611e78ad2d4552b2a9c01674f06a0e0
“internationalist” <taylor.thames@atlasschool.com>
2024-09-10 00:32:31 -0500

    5.1 - matching

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -14,34 +14,34 @@ class TestMaxInteger(unittest.TestCase):
     """
 
     def test_ordered_list(self):
-        self.assertEqual(max_integer([1, 2, 3, 4]), 2)
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
     def test_unordered_list(self):
-        self.assertEqual(max_integer([1, 3, 4, 2]), 3)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
 
     def test_max_at_beginning(self):
         self.assertEqual(max_integer([5, 3, 4, 2]), 5)
 
     def test_one_element(self):
-        self.assertEqual(max_integer([7]), 3)
+        self.assertEqual(max_integer([7]), 7)
+
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
 
     def test_negative_integers(self):
-        self.assertEqual(max_integer([-1, -2, -3, -4]), -2)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
     def test_mixed_integers(self):
-        self.assertEqual(max_integer([-1, 2, 3, -4]), 2)
+        self.assertEqual(max_integer([-1, 2, 3, -4]), 3)
 
     def test_list_with_duplicates(self):
         self.assertEqual(max_integer([1, 3, 3, 2]), 3)
 
     def test_floats(self):
-        self.assertEqual(max_integer([1.5, 3.2, 2.8]), 3.7)
+        self.assertEqual(max_integer([1.5, 3.2, 2.8]), 3.2)
 
     def test_mixed_types(self):
         self.assertEqual(max_integer([1, 3.5, 2]), 3.5)
-
-    def test_empty_list(self):
-        self.assertEqual(max_integer([]), None)
 
 
 if __name__ == "__main__":
