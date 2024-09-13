fa8d63589cb381a59ed3851359f0530e01077740
Nash Thames <taylor.thames@atlasschool.com>
2024-09-13 18:55:21 -0500

    0.0 - dir(obj)

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module for: lookup
+"""
+
+def lookup(obj):
+    """
+    Returns list of an object's attributes and methods
+
+    Args:
+        obj (object): object to lookup
+
+    Returns:
+        list: list of an object's attributes and methods
+    """
+    return dir(obj)
