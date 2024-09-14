6c43179fc4928129d08342dcd11153a40af740ec
Nash Thames <taylor.thames@atlasschool.com>
2024-09-13 21:49:04 -0500

    4.1 - uses issubclass

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 """
-Module checks if an object is an instance of a class
-that inherited from a specified class
+Module checks if an object clas is a subclass of a specified class
 """
 
 
 def inherits_from(obj, a_class):
+    """
+    Uses issubclass() method
+
+    Args:
+        obj (object):       object to check
+        a_class (class):    class to check against
+
+    Returns:
+        True (bool):    True if object is an instance of the class
+        False (bool):   False otherwise
+
+    """
+    return issubclass(type(obj), a_class)
