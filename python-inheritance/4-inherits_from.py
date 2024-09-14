49bf8e3370c17dc8ce9047e6f6c7427d26557493
Nash Thames <taylor.thames@atlasschool.com>
2024-09-13 21:50:38 -0500

    4.2 - need type(obj) to check if not same class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -17,4 +17,4 @@ def inherits_from(obj, a_class):
         False (bool):   False otherwise
 
     """
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
