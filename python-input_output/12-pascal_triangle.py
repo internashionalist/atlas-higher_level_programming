5c57e3aa57cc5c2fb9efa5dd3c0b0628ade0667c
Nash Thames <taylor.thames@atlasschool.com>
2024-09-14 08:16:13 -0500

    12.2 - that was fun to be honest

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -8,5 +8,16 @@ def pascal_triangle(n):
     """
     Returns a matrix representing a 'Pascal’s Triangle' of n size
     """
-
+    if n <= 0:
+        return []
     
+    Pascals = [[1]]  # first row is always [1]
+    if n > 1:
+        Pascals.append([1, 1])  # second row is always [1, 1]
+        for i in range(2, n):  # now it gets fun
+            row = [1]  # first element
+            for k in range(1, i):  # middle elements
+                row.append(Pascals[i - 1][k - 1] + Pascals[i - 1][k])  # sum of 2 above
+            row.append(1) # last element
+            Pascals.append(row)  # add completed row
+    return Pascals  # return Pascals Triangle!
