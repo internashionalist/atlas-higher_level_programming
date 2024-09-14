13f1a4aa8067cb09bdbba18a922912a7e98e1478
Nash Thames <taylor.thames@atlasschool.com>
2024-09-14 05:52:16 -0500

    1.1 - this took some googling

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,3 +5,9 @@ Module for write_file
 
 
 def write_file(filename="", text=""):
+    """
+    method writes string to a UTF-8 text file
+    and returns number of characters written
+    """
+    with open(filename, mode="w", encoding="utf-8") as file:
+        return file.write(text)
