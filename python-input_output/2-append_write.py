719bf989aeeebf2de6e2fd7a3aaac9abd0783c22
Nash Thames <taylor.thames@atlasschool.com>
2024-09-14 05:59:42 -0500

    2.1 - once you get the modes, it's not that hard

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,3 +5,16 @@ Module for append_write
 
 
 def append_write(filename="", text=""):
+    """
+    Method appends string to a UTF-8 text file
+    and returns number of characters added
+
+    Args:
+        filename (str): file to append to
+        text (str): text to append to file
+
+    Returns:
+        int: number of characters added
+    """
+    with open(filename, mode="a", encoding="utf-8") as file:
+        return file.write(text)
