5e7faa3434460649a2a0fef91a61a1e613816811
Nash Thames <taylor.thames@atlasschool.com>
2024-09-13 21:57:40 -0500

    6.0 - no method, one exception raised

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module defines a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Prototype for BaseGeometry class
+    """
+    def area(self):
+        """
+        TBD area method
+
+        Raises:
+            Exception: area() is not implemented
+
+        Returns
+            None
+        """
+        raise Exception("area() is not implemented")
