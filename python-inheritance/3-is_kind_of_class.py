bead67267bbd756f2694eaed1346db054040ccb8
Nash Thames <taylor.thames@atlasschool.com>
2024-09-13 21:43:35 -0500

    3.1 - uses isinstance

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -5,3 +5,16 @@ Module checks if an object is an instance of a class
 
 
 def is_kind_of_class(obj, a_class):
+    """
+    Uses isinstance() to check if an object is an instance of a class
+
+    Args:
+        obj (object):       object to check
+        a_class (class):    class to check against
+
+    Returns:
+        True (bool):    True if object is an instance of the class
+        False (bool):   False otherwise
+
+    """
+    return isinstance(obj, a_class)
