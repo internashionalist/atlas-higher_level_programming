aee0a81ac9ba5a159b4ee39675df8ec34ae3e622
Nash Thames <taylor.thames@atlasschool.com>
2024-09-13 19:16:00 -0500

    1.0 - has tests

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+Module adds print_sorted method to MyList class
+"""
+
+
+class MyList(list):
+    """
+    inherits from list
+
+    Attributes:
+        list (list): list to inherit from
+
+    Methods:
+        print_sorted: prints the list sorted
+
+    Returns:
+        None
+    """
+
+    def print_sorted(self):
+        """
+        Prints list in ascending order using sorted()
+        """
+        print(sorted(self))  # sorted() returns a new list
