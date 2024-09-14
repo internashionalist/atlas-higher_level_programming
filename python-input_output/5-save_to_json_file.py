e0490b25fac6447ca5a7ef8bb7d79e99577ba0ca
Nash Thames <taylor.thames@atlasschool.com>
2024-09-14 06:32:04 -0500

    5.1 - dumps and write

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,3 +6,15 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """
+    Method writes an object to a text file using JSON representation
+
+    Args:
+        my_obj (object): object to write to file
+        filename (str): name of file to write to
+
+    Returns:
+        None
+    """
+    with open(filename, mode='w', encoding='utf-8') as file:
+        file.write(json.dumps(my_obj))
