eced9c03d608464c4481f9df2184c68465b67ee3
Nash Thames <taylor.thames@atlasschool.com>
2024-09-14 06:52:43 -0500

    8.0 - base

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for class_to_json
+"""
+
+
+def class_to_json(obj):
+    """
+    Function returns dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object
+
+    Args:
+        obj (object): object to serialize
+
+    Returns:
+        dict: dictionary representation of object
+    """
+    return obj.__dict__
