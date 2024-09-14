0676522ea6f98763c053812799136092d0d15402
Nash Thames <taylor.thames@atlasschool.com>
2024-09-14 06:09:55 -0500

    3.1 - haha 'dumps'

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -6,3 +6,13 @@ import json
 
 
 def to_json_string(my_obj):
+    """
+    Method returns JSON representation of an object
+
+    Args:
+        my_obj (object): object to convert to JSON
+
+    Returns:
+        str: JSON representation of object
+    """
+    return json.dumps(my_obj)  # "dumps" FTW
