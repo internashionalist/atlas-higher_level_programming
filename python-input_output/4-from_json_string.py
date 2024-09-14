d3ba19aac280e61aeec76a3bec3ac7ee73331178
Nash Thames <taylor.thames@atlasschool.com>
2024-09-14 06:15:47 -0500

    4.1 - loads

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -6,3 +6,13 @@ import json
 
 
 def from_json_string(my_str):
+    """
+    Method uses loads and returns Python object represented by JSON string
+
+    Args:
+        my_str (str): JSON string to convert to Python object
+
+    Returns:
+        object: Python object represented by JSON string
+    """
+    return json.loads(my_str)
