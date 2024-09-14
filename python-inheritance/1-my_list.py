ebc26d0412ab44a8a32f42c826f02b532c241429
Nash Thames <taylor.thames@atlasschool.com>
2024-09-13 19:54:01 -0500

    1.5 - trying to return unchanged list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -16,11 +16,13 @@ class MyList(list):
         print_sorted: prints the list sorted
 
     Returns:
-        None
+        sorted list
     """
 
     def print_sorted(self):
         """
         Prints list in ascending order using sorted()
         """
-        print(sorted(self))  # sorted() returns a new list
+        sorted_list = sorted(self)  # sorted() returns a new list
+        print(sorted(self))  # print the new list
+        return sorted_list  # return the new list
