e3c6653682c8e4b35231e4d536dc9a540c48069c
Nash Thames <taylor.thames@atlasschool.com>
2024-09-13 20:44:40 -0500

    2.0 - bool for class comp

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module checks if an object has a specified class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Compares an object's class to a specified class
+
+    Args:
+        obj (object):       object to check
+        a_class (class):    class to check against
+
+    Returns:
+        True (bool):    True if object is an instance of the class
+        False (bool):   False otherwise
+    """
+    return type(obj) == a_class
