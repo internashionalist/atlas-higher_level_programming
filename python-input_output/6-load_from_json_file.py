50bbdd94a27c1a0c27df482441f74bfce15bab80
Nash Thames <taylor.thames@atlasschool.com>
2024-09-14 06:39:23 -0500

    6.1 - load

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,3 +6,14 @@ import json
 
 
 def load_from_json_file(filename):
+    """
+    Function creates an object from a JSON file
+
+    Args:
+        filename (str): file from which to extract object
+
+    Returns:
+        object: object represented by the JSON file
+    """
+    with open(filename, encoding="utf-8") as file:
+        return json.load(file)
