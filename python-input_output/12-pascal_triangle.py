2ed589c85686f6737ca4ca0555dfba75fef55c13
Nash Thames <taylor.thames@atlasschool.com>
2024-09-14 08:18:27 -0500

    12.2 - pycode

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -10,14 +10,14 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    
+
     Pascals = [[1]]  # first row is always [1]
     if n > 1:
         Pascals.append([1, 1])  # second row is always [1, 1]
         for i in range(2, n):  # now it gets fun
             row = [1]  # first element
-            for k in range(1, i):  # middle elements
-                row.append(Pascals[i - 1][k - 1] + Pascals[i - 1][k])  # sum of 2 above
-            row.append(1) # last element
+            for k in range(1, i):  # middle elements - sum of two above
+                row.append(Pascals[i - 1][k - 1] + Pascals[i - 1][k])
+            row.append(1)  # last element
             Pascals.append(row)  # add completed row
     return Pascals  # return Pascals Triangle!
