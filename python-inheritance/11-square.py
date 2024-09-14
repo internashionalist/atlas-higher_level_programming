acec8601ae0a9e7a28fbbe823fd1ade8564c6bc1
Nash Thames <taylor.thames@atlasschool.com>
2024-09-13 23:40:31 -0500

    11.0 - show square

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Module defines a child class Square that inherits 9-rectangle
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    Prototype for Square class
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square object that inherits 9-rectangle
+
+        Arguments:
+            size (int): size of square
+
+        Methods:
+            integer_validator:  inherited
+            area:               inherited
+
+        Returns:
+            None
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates area of square
+
+        Returns:
+            Area of square
+        """
+        return self.__size**2
+
+    def __str__(self):
+        """
+        Returns representation of square
+
+        Returns:
+            String representation of square
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
