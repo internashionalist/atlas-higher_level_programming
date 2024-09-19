560fb2b88b22c7c7b2f98744a5fe6888b7ad013e
Nash Thames <taylor.thames@atlasschool.com>
2024-09-19 17:30:12 -0500

    base.6 - not sure if all this is necessary but it works

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -101,3 +101,22 @@ class Base:
             dummy_inst = cls(1)  # create dummy - default 1x1
         dummy_inst.update(**dictionary)  # update w dict attributes
         return dummy_inst  # return new instance
+    
+    @classmethod
+    def load_from_file(cls):
+        """
+        This method returns a list of instances from a file
+
+        Returns:
+            inst_list: list of instances
+        """
+        filename = cls.__name__ + ".json"  # class name + .json
+        inst_list = []  # list to store instances (empty for now)
+        if os.path.exists(filename):  # if file exists
+            with open(filename, "r") as f:  # open in read mode
+                json_lists = cls.from_json_string(f.read())  # read JSON string
+            for json_dict in json_lists:  # iterate through list
+                inst_list.append(cls.create(**json_dict))  # append instances
+        else:  # if file does not exist
+            pass  # 
+        return inst_list  # return list of instances (empty if no file)
