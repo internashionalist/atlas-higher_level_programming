76b7046b2c8b9008300f422445a43bfe02831d9d
Nash Thames <taylor.thames@atlasschool.com>
2024-09-20 12:12:01 -0500

    testRec.32 - revert

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -170,7 +170,8 @@ class TestRectangle(unittest.TestCase):
         Test save_to_file method with empty list
         """
         Rectangle.save_to_file([])
-
+        with open("Rectangle.json", "r") as file:
+            self.assertEqual(file.read(), "[]")
 
     def test_save_to_file_None(self):
         """
