88098fbbde70ebfcaa77f47a071590b811aff58b
Nash Thames <taylor.thames@atlasschool.com>
2024-09-19 21:57:42 -0500

    test.10 - from_json_string with None and empty

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -53,3 +53,15 @@ class TestBase(unittest.TestCase):
         Test to_json_string with empty list
         """
         self.assertEqual(Base.to_json_string([]), "[]")
+
+    def test_from_json_string_None(self):
+        """
+        Test from_json_string with None
+        """
+        self.assertEqual(Base.from_json_string(None), [])
+
+    def test_from_json_string_empty(self):
+        """
+        Test from_json_string with empty string
+        """
+        self.assertEqual(Base.from_json_string(""), [])
