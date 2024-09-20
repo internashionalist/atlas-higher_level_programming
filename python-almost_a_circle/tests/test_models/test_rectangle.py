43ef995dd40ed8ee9523d3c04969facdf61f1801
Nash Thames <taylor.thames@atlasschool.com>
2024-09-19 22:08:17 -0500

    testRec.2 - valid case

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -9,3 +9,16 @@ from models.rectangle import Rectangle
 from models.square import Square
 
 
+class TestRectangle(unittest.TestCase):
+    """
+    TestRectangle class
+    """
+    def test_valid_attributes(self):
+        """
+        Test valid attributes
+        """
+        rectangle = Rectangle(2, 4, 6, 8)
+        self.assertEqual(rectangle.width, 2)
+        self.assertEqual(rectangle.height, 4)
+        self.assertEqual(rectangle.x, 6)
+        self.assertEqual(rectangle.y, 8)
