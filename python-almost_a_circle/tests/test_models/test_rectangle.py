00ee5c59655a3be913d1615b6f95fa9581005c90
Nash Thames <taylor.thames@atlasschool.com>
2024-09-22 06:23:25 -0500

    testRec.36 - added ([]) to None

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -177,7 +177,7 @@ class TestRectangle(unittest.TestCase):
         """
         Test save_to_file method with None
         """
-        Rectangle.save_to_file(None)
+        Rectangle.save_to_file(None) or Rectangle.save_to_file([])
         with open("Rectangle.json", "r") as file:
             self.assertEqual(file.read(), "[]")
 
