80b9d2dd47c3f6329d707ac8bfdef9891c140e4d
Nash Thames <taylor.thames@atlasschool.com>
2024-10-11 20:09:08 -0500

    9 - create and add rows

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- creates second_table in database hbtn_0c_0 in MySQL server, then adds multiples rows
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+INSERT INTO second_table(id, name, score)
+VALUES 
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8)
+;
