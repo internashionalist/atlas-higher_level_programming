5c49e62486f6601776bbdab75d0ebf93a194e9a9
Nash Thames <taylor.thames@atlasschool.com>
2024-10-11 20:19:31 -0500

    12.0 - update set where

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- updates score of Bob to 10 in second_table in hbtn_0c_0 database in the mySQL server
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- removes all records with a score of 5 or lower in second_table in hbtn_0c_0 database in the mySQL server
+DELETE FROM second_table
+WHERE score <= 5;
