df8aa5cb1e77494a59a43583a171f1d1c45f6cf3
Nash Thames <taylor.thames@atlasschool.com>
2024-10-13 20:57:29 -0500

    0.0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This module contains the list_states function.
+"""
+
+import MySQLdb
+import sys
+
+
+def list_states():
+    """
+    Function lists all states from database hbtn_0e_0_usa
+
+    """
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    list_states()
