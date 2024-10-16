03629b3d125dc43f59f40bb1db38834e54fe2758
Nash Thames <taylor.thames@atlasschool.com>
2024-10-15 20:27:14 -0500

    9.2 - pycode

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -23,8 +23,9 @@ def states_with_a():
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for item in session.query(State).filter(State.name.like("%a%")).order_by(State.id):
-        print("{}: {}".format(item.id, item.name))
+    for item in session.query(State).filter(
+        State.name.like("%a%")).order_by(State.id):
+            print("{}: {}".format(item.id, item.name))
 
     session.close()
 
