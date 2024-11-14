03067f8dcaae64a07a247dbae5b5161c02678842
Nash Thames <taylor.thames@atlasschool.com>
2024-11-14 00:50:45 -0600

    2.4 - semistandard

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,8 +3,7 @@
 const myVar = ['No argument', 'Argument found', 'Arguments found'];
 
 if (process.argv.length > 3) {
-    console.log(myVar[2]);
-} 
-else {
-    console.log(myVar[process.argv.length - 2]);
+  console.log(myVar[2]);
+} else {
+  console.log(myVar[process.argv.length - 2]);
 }
