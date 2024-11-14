0fc2632e754991bf2b545e9f542c45c8b8292b2d
Nash Thames <taylor.thames@atlasschool.com>
2024-11-14 00:49:17 -0600

    2.2 - new method

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,12 +2,9 @@
 
 const myVar = ['No argument', 'Argument found', 'Arguments found'];
 
-if (process.argv.length > 1) {
+if (process.argv.length > 3) {
     console.log(myVar[2]);
 } 
-else if (process.argv.length === 1) {
-    console.log(myVar[1]);
-}
 else {
-    console.log(myVar[0]);
+    console.log(myVar[process.argv.length - 2]);
 }
