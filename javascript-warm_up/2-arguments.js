1621277c4b047dca8d30bf3707b323f3f323e2b4
Nash Thames <taylor.thames@atlasschool.com>
2024-11-14 00:45:24 -0600

    2.1 - I can't count

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,10 +2,10 @@
 
 const myVar = ["No argument", "Argument found", "Arguments found"];
 
-if (process.argv.length > 2) {
+if (process.argv.length > 1) {
     console.log(myVar[2]);
 } 
-else if (process.argv.length === 2) {
+else if (process.argv.length === 1) {
     console.log(myVar[1]);
 }
 else {
