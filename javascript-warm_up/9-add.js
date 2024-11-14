fa1e9a3b28547b453c066b978103995510d57ec4
Nash Thames <taylor.thames@atlasschool.com>
2024-11-14 08:42:02 -0600

    7, 8, 9 - all run

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+function add (a, b) {
+  if (!isNaN(a) && !isNaN(b)) {
+    console.log(a + b);
+  } else {
+    console.log('NaN');
+  }
+}
